99686317efee084c61803ca4e6ad4868327e4015
oralcoder <oralcoder@gmail.com>
2025-10-20 02:26:33 +0900

    db FK 제약

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 DATABASE_URL = "sqlite:///./mes.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_conn, _):
+    cur = dbapi_conn.cursor()
+    cur.execute("PRAGMA foreign_keys=ON")
+    cur.close()
 
 def get_db():
     db = SessionLocal()
