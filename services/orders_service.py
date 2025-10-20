a775fb9a9e116d44d353c548468de840693f9fdc
oralcoder <oralcoder@gmail.com>
2025-10-21 00:52:56 +0900

    remove type hint for test

--- services/orders_service.py
+++ services/orders_service.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
 from models import Order
 
-def create_order(db: Session, order_name: str, product_code: str):
+def create_order(db, order_name, product_code):
     new_order = Order(order_name=order_name, product_code=product_code)
     db.add(new_order)
     db.commit()
     db.refresh(new_order)
     return new_order
 
-def get_all_orders(db: Session):
+def get_all_orders(db):
     return db.query(Order).all()
 
